858c429f94da6c7e4fec14faa65ea635ef6e7c13
Michael Nickey <mnickey@gmail.com>
2025-08-14 22:58:00 -0700

    Create physcialproduct.py

---
+++ Advanced OOP Concepts/Mixins/physcialproduct.py
@@ -0,0 +1,9 @@
+# TODO: Import the required classes and mixins
+from product import Product
+from shippablemixin import ShippableMixin
+
+class PhysicalProduct(Product, ShippableMixin):
+    # TODO: Make this class inherit from Product and ShippableMixin
+    # TODO: Modify the class definition to: class PhysicalProduct(Product, ShippableMixin):
+    # TODO: No need to override any methods as it inherits all needed functionality
+    pass
