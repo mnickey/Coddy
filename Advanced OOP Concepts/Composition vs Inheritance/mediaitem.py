c9cf6d86198e44d510697281cc62e631318bd34c
Michael Nickey <mnickey@gmail.com>
2025-08-14 22:43:50 -0700

    Create mediaitem.py

---
+++ Advanced OOP Concepts/Composition vs Inheritance/mediaitem.py
@@ -0,0 +1,8 @@
+class MediaItem:
+    def __init__(self, title, creator, year):
+        # TODO: Initialize the MediaItem class with title, creator, and year parameters
+        # TODO: Store these parameters as instance attributes
+        # NOTE: This class will be used for composition approach
+        self.title = title
+        self.creator = creator
+        self.year = year 
