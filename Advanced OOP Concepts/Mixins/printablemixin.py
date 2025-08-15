b7b802859a80a36f874806bc2287da0fd2a94f58
Michael Nickey <mnickey@gmail.com>
2025-08-14 23:00:09 -0700

    Create printablemixin.py

---
+++ Advanced OOP Concepts/Mixins/printablemixin.py
@@ -0,0 +1,6 @@
+class PrintableMixin:
+    def print_details(self):
+        # TODO: Implement the print_details method that returns product details
+        # TODO: Return a formatted string with product name and price
+        # TODO: Use f-string format: "Product: {self.name}, Price: ${self.price}"
+        return f"Product: {self.name}, Price: ${self.price}"
