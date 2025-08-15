0422840f7a282780b0319c5bb076268ca0fc4c14
Michael Nickey <mnickey@gmail.com>
2025-08-14 22:41:47 -0700

    Create media.py

---
+++ Advanced OOP Concepts/Composition vs Inheritance/media.py
@@ -0,0 +1,12 @@
+class Media:
+    def __init__(self, title, creator, year):
+        # TODO: Initialize the Media class with title, creator, and year parameters
+        # TODO: Store these parameters as instance attributes
+        self.title = title
+        self.creator = creator
+        self.year = year
+    
+    def display_info(self):
+        # TODO: Define a base display_info method that will be overridden by child classes
+        # NOTE: This method should be implemented by child classes
+        pass
