f78f58394e29b2f4cf4d8beba5900ac4f6044da1
Michael Nickey <mnickey@gmail.com>
2025-08-15 19:35:42 -0700

    Update person_creator.py

--- Advanced OOP Concepts/Advanced Arguments/The **kwarg/person_creator.py
+++ Advanced OOP Concepts/Advanced Arguments/The **kwarg/person_creator.py
@@ -3,4 +3,7 @@ def create_person(**kwargs):
     # TODO: Print "Person created with properties:"
     # TODO: Loop through each key-value pair in the dictionary
     # TODO: Print each key-value pair on a new line in the format "key: value"
-    pass
+    details = kwargs
+    print("Person created with properties:")
+    for key, value in details.items():
+        print(f"{key}: {value}")
